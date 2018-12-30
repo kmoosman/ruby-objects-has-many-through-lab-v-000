a5779b58b24e82ad4286be36e05811fbdd84b470
Katie Moosman <moosman.katie@gmail.com>
2018-12-30 21:09:02 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,6 +14,10 @@ class Patient
     @@all
   end
   
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+  
   # def new_appointment(doctor, date)
   #   Appointment.new(self, date, doctor)
   # end
