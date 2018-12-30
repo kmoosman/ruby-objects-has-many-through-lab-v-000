fdea8706d4b1eeb3a4a8ee6f4754da83c2d1272b
Katie Moosman <moosman.katie@gmail.com>
2018-12-30 21:21:48 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,13 +17,13 @@ class Doctor
 #     Appointment.new(date, patient, self)
 #   end
   
-#   def appointments
-#     Appointment.all.select {|appointment| appointment.doctor == self}
-#   end
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
   
-#   def patients
-#     appointments.map {|appointment| appointment.patient}
-#   end
+  def patients
+    appointments.map {|appointment| appointment.patient}
+  end
 
 	   attr_accessor :name
    @@all = []
@@ -33,13 +33,13 @@ class Doctor
      @@all << self
    end
  
-    def appointments
-     Appointment.all.select {|appointment| appointment.doctor == self}
-   end
+  #   def appointments
+  #   Appointment.all.select {|appointment| appointment.doctor == self}
+  # end
  
-    def patients
-     appointments.map {|appointment| appointment.patient}
-   end
+  #   def patients
+  #   appointments.map {|appointment| appointment.patient}
+  # end
  
     def self.all
      @@all
