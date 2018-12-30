f1bb5804c93cd7cf54fc8a361becfba561e7d30e
Katie Moosman <moosman.katie@gmail.com>
2018-12-30 21:20:24 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -25,5 +25,30 @@ class Patient
 #     appointments.map {|appointment| appointment.doctor}
 #   end
 
+   attr_accessor :name, :appointment, :doctor
+   @@all = []
+ 
+    def initialize(name)
+     @name = name
+     @@all << self
+   end
+ 
+    def self.all
+     @@all
+   end
+ 
+    def appointments
+     Appointment.all.select {|appointment| appointment.patient == self}
+   end
+ 
+    def doctors
+     appointments.collect {|appointment| appointment.doctor}
+   end
+ 
+    def new_appointment(doctor, date)
+     Appointment.new(self, date, doctor)
+   end
+ 
+
 
 end
